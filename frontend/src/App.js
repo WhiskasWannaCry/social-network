9d8ed04b82d9712165dd1f9d5f0ad3ae7e582e68
Karim Zerman <whiskaswannacry@gmail.com>
2024-01-08 13:47:30 +0200

    Init

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import styled from "styled-components"
+
+const Container = styled.div`
+
+`
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello world</h1>
+    </Container>
+  );
+}
+
+export default App;
