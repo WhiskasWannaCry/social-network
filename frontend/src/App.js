f7d7f628f2149bfcfc1c60c84889b745dbfbce94
Karim Zerman <whiskaswannacry@gmail.com>
2024-01-24 17:52:07 +0200

    Login and Sign Up pages created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,30 @@ import { useEffect, useState } from "react";
 import Feed from "./pages/Feed/Feed";
 import EditProfile from "./pages/EditProfile/EditProfile";
 import { useSelector } from "react-redux";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   background-color: ${(props) => props.theme.mainBg};
+  min-height: 100%;
+  width: 100%;
+  max-width: 1248px;
 `;
 
 const Body = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 64px;
+  margin-top: ${({pathname}) => pathname != "/auth/login" && pathname != "/auth/registration" && "64px"};
   gap: 12px;
   margin-bottom: 12px;
+  min-height: 100%;
 `;
 
 const ContentContainer = styled.div`
 width: 60%;
+height: 100%;
 `;
 
 function App() {
@@ -33,19 +40,22 @@ function App() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    location.pathname === "/" && navigate("/profile");
+    location.pathname === "/" && navigate("/auth/login");
   }, []);
   return (
     <ThemeProvider theme={theme}>
     <Container>
-      <Header theme={theme}></Header>
-      <Body>
-        <MainNavigation></MainNavigation>
+      {(location.pathname !== "/auth/login" && location.pathname !== "/auth/registration") && <Header theme={theme}></Header>}
+      <Body pathname={location.pathname}>
+        {(location.pathname !== "/auth/login" && location.pathname !== "/auth/registration") && <MainNavigation></MainNavigation>}
         <ContentContainer>
           <Routes>
             <Route path="/profile" element={<Profile></Profile>}></Route>
             <Route path="/feed" element={<Feed></Feed>}></Route>
             <Route path="/edit" element={<EditProfile></EditProfile>}></Route>
+            <Route path="/auth/login" element={<Login></Login>}></Route>
+            <Route path="/auth/registration" element={<SignUp></SignUp>}></Route>
+
           </Routes>
         </ContentContainer>
       </Body>
---
+++ frontend/src/pages/Auth/Login.js
@@ -0,0 +1,102 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  border: ${(props) => props.theme.mainBlockBorder};
+  border-radius: 8px;
+  padding: 8px 16px 8px 16px;
+  width: 50%;
+  height: 400px;
+  gap: 12px;
+`;
+
+const LoginTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme.mainTextColor};
+  width: 100%;
+  height: 20%;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  width: 100%;
+  height: 30%;
+`;
+
+const Input = styled.input`
+  background-color: ${(props) => props.theme.mainBg};
+  border: ${(props) => props.theme.mainBlockBorder};
+  border-radius: 8px;
+  padding: 8px;
+  width: 100%;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const Btn = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) =>
+    (props.isSignUpBtn && props.theme.greenBtnBg) || props.theme.btnBg};
+  padding: 8px;
+  border-radius: 8px;
+  width: 100%;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  &:hover {
+    background-color: ${(props) => props.theme.hoverBtnBg};
+  }
+`;
+
+const HaveNotAccountTitle = styled.span`
+  width: 100%;
+  font-size: 12px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+`;
+
+const Login = () => {
+  const navigate = useNavigate()
+  return (
+    <Container>
+      <LoginContainer>
+        <LoginTitle>Log In</LoginTitle>
+        <InputsContainer>
+          <Input type="phone" placeholder="Email"></Input>
+          <Input type="password" placeholder="Password"></Input>
+        </InputsContainer>
+        <Btn>Log In</Btn>
+        <HaveNotAccountTitle>Haven't account?</HaveNotAccountTitle>
+        <Btn isSignUpBtn={true} onClick={() => navigate("/auth/registration")}>Sign Up</Btn>
+      </LoginContainer>
+    </Container>
+  );
+};
+
+export default Login;
