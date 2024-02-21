582bdcfee40de3d287c3eea0c61fdec77a8401e1
Karim Zerman <whiskaswannacry@gmail.com>
2024-02-21 17:51:19 +0200

    Rewrite user model

--- backend/models.js
+++ backend/models.js
@@ -3,26 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  surname: { type: String, required: true },
-  age: String,
-  avatar: String,
-  background: String,
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: String },
+  primary: {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    dateOfBirth: { type: String },
+    email: { type: String, required: true },
+    website: { type: String},
+    description: { type: String },
+  },
+  images: {
+    avatar: { type: String, required: true },
+    background: { type: String, required: true },
+  },
+  socialContacts: {
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  secret: {
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+  },
 });
 
 const chatSchema = new Schema({
-  senderID: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  recipientID: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  senderID: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  recipientID: [{ type: Schema.Types.ObjectId, ref: "User" }],
   messages: [
     {
       text: { type: String, required: true },
       id: { type: String, required: true },
       date: { type: String, required: true },
-      senderID: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-      recipientID: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      senderID: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+      recipientID: [
+        { type: Schema.Types.ObjectId, ref: "User", required: true },
+      ],
     },
   ],
 });
